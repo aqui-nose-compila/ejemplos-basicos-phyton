a8e4474591c477eef583c1353ab39c1ec2a7c66e
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-02-26 18:37:32 +0100

    012-asignar-valor-a-multiples-variables.py

---
+++ 012-asignar-valor-a-multiples-variables.py
@@ -0,0 +1,9 @@
+# Programa que asigna un valor a 3 variables diferentes y lo muestra por pantalla
+
+# creación y asignación del valor "Valor" a 3 variables diferentes
+variable1 = variable2 = variable3 = "Valor"
+
+# Mostrar el valor de cada variable para comprobar que tienen el mismo valor "Valor"
+print("Variable1: " + variable1)
+print("Variable2: " + variable2)
+print("Variable3: " + variable3)
