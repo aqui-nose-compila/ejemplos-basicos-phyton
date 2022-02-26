347d352cdbb175232654a5bd82bea03adc8f6159
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-02-26 11:01:43 +0100

    006-operaciones-matematicas-basicas.py

---
+++ 006-operaciones-matematicas-basicas.py
@@ -0,0 +1,27 @@
+# Programa que calcula varias operaciones matemáticas básicas con dos variables
+# Las operaciones a realizar serán: suma, resta, multiplicación y división
+# Al final se mostrará el resultado de cada operación en una línea
+
+# Creación y asignación de valores a las variables que contienen los números
+numero1 = 10
+numero2 = 2
+
+# Realización de las diferentes operaciones y guardado de cada operación en una variable
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+# Mostrar el resultado de cada operación en una sentencia diferente junto con texto
+# Suma
+print('La suma de ', numero1, ' y ', numero2, ' es: ', suma)
+
+# Resta
+print('La resta de ', numero1, ' y ', numero2, ' es: ', resta)
+
+# Multiplicacion
+print('La multiplicación de ', numero1,
+      ' y ', numero2, ' es: ', multiplicacion)
+
+# División
+print('La división de ', numero1, ' y ', numero2, ' es: ', division)
