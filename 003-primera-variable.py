7a79ba9e99f1c34451536e525d85c807ec9b7dd4
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-02-26 09:43:11 +0100

    003-primera-variable.py

---
+++ 003-primera-variable.py
@@ -0,0 +1,7 @@
+# Programa que crea una variable, le asigna un valor y muestra el contenido de la variable.
+
+# Crear la variable "numero" y asignación del valor 10
+numero = 10
+
+# Mostra el contenido de la variable
+print(numero)
