d133672244a2320ddb95f4737b2b5dac05be8eed
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-02-26 09:46:11 +0100

    004-suma-de-dos-numeros-con-variables.py

---
+++ 004-suma-de-dos-numeros-con-variables.py
@@ -0,0 +1,8 @@
+# Programa que muestra la suma del contenido de dos variables
+
+# Creación y asignación del valor de las dos variables
+numero1 = 7
+numero2 = 5
+
+# Mostrar el resultado de la suma
+print(numero1 + numero2)
