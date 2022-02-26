1776fb2289ea456dea2062becdf1390b85b425bc
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-02-26 18:43:33 +0100

    013-concatenacion-texto-variable.py

---
+++ 013-concatenacion-texto-variable.py
@@ -0,0 +1,12 @@
+# Programa que concatena el contenido de tres variables de tipo cadena y muestra por pantalla el resultado
+
+# Creación de las tres variables y asignación de valores
+cadena1 = "Hola "
+cadena2 = "don Pepito. "
+cadena3 = "don José."
+
+# Concatenación de las variables string y guardado en otra variable
+saludo = cadena1 + cadena2 + cadena1 + cadena3
+
+# Mostrar el saludo completo
+print(saludo)
