e2f5f2107f08d39c7de2a6aa7245139e237456c5
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-02-26 09:54:40 +0100

    005-mostrar-variables-y-texto.py

---
+++ 005-mostrar-variables-y-texto.py
@@ -0,0 +1,11 @@
+# Programa que realiza la suma del contenido de dos variables y muestra el resultado
+# El resultado lo muestra junto con una cadena de texto
+
+# Declaración y asignación del valor de las variables
+numero1 = 4
+numero2 = 8
+
+# Guardar la suma en otra variable
+suma = numero1 + numero2
+# Mostrar el resultado de la suma junto con un texto con la función print
+print('La suma de ', numero1, ' y ', numero2, ' es ', suma)
